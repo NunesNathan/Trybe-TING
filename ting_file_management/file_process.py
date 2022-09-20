9e5c6d0e68b4cdac9537e618e5dc13f9b70767ad
NunesNathan <nunesxd7@hotmail.com>
2022-09-20 16:06:19 -0300

    feat: process works

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management import file_management
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text_list = file_management.txt_importer(path_file)
+
+    if path_file in instance.data:
+        return False
+
+    instance.enqueue(path_file)
+
+    result = str({
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text_list),
+        "linhas_do_arquivo": text_list
+    })
+
+    sys.stdout.write(result)
 
 
 def remove(instance):
