e213b80279052fa56eae86d2009c7732fcc6df46
NunesNathan <nunesxd7@hotmail.com>
2022-09-20 13:17:37 -0300

    feat: txt importer works

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    data = list()
+
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file) as file:
+                lines = file.readlines()
+
+                for line in lines:
+                    data.append(line.strip("\n"))
+
+            return data
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido\n")
